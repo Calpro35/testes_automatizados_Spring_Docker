854001f126d45178e15f1db28d722f995b634311
Carlos Oliveira <126629076+Calpro35@users.noreply.github.com>
2025-10-26 21:03:31 -0300

    Update and rename Hook to Hook.java

--- src/test/java/hook/Hook.java
+++ src/test/java/hook/Hook.java
@@ -5,27 +5,27 @@ import io.cucumber.java.Before;
 import io.cucumber.java.BeforeAll;
 
 
-public class Hook {
+class Hook {
     @BeforeAll
-    public static void setUpBeforeClass() {
+    static void setUpBeforeClass() {
         // Simulação de configuração global antes da execução de todos os testes
         System.out.println("Configuração global antes de todos os testes.");
         inicializarAmbiente();
     }
     @AfterAll
-    public static void tearDownAfterClass() {
+    static void tearDownAfterClass() {
         // Simulação de limpeza global após a execução de todos os testes
         System.out.println("Limpeza global após todos os testes.");
         limparAmbiente();
     }
     @Before
-    public void setUp() {
+    void setUp() {
         // Simulação de configuração antes da execução de cada cenário
         System.out.println("Iniciando um novo cenário de teste...");
         prepararDadosParaTeste();
     }
     @After
-    public void tearDown() {
+    void tearDown() {
         // Simulação de limpeza após a execução de cada cenário
         System.out.println("Finalizando o cenário de teste...");
         limparDadosDepoisDoTeste();
